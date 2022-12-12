d195a44840fce67cb87f350e7e8a6f0ef9fef9b3
Victor Nadalini <victornadalini95@gmail.com>
2022-12-12 13:36:58 -0300

    primeiro commit

---
+++ projeto_alcool_ou_gasolina/lib/main.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/material.dart';
+import 'package:projeto_alcool_ou_gasolina/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
